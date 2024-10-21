ec16e5ce540b154ec88eecd4776a9f5ad302331a
Matteo Piccini <gnappo.1@live.it>
2024-10-21 09:30:19 -0700

    add starter for live coding

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,24 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    
+    (() => fetch("http://localhost:8001/transactions")
+      .then((response) => response.json())
+      .then(setTransactions)
+      .catch((error) => console.error(error))
+    )()
+  }, []);
+
   return (
     <div>
       <Search />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transactions={transactions}/>
     </div>
   );
 }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-function TransactionsList() {
+function TransactionsList({ transactions }) {
+  const mappedTransactions = transactions.map((transaction) => (
+    <Transaction key={transaction.id} transaction={transaction} />
+  ));
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +23,7 @@ function TransactionsList() {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}
+        {mappedTransactions}
       </tbody>
     </table>
   );
